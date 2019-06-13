fcf8f9b3c7cf0a3aedc5f9c44a4d5bed56cc24da
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-06-12 21:05:54 -0700

    initial commit

---
+++ example-app/.nomad-codecheckrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+	"rules": [
+		"no-assets-outside-assets-folder",
+		"no-network-request-outside-network-managers-folder",
+		"no-storage-outside-persistent-data-managers-folder",
+	]
+}
